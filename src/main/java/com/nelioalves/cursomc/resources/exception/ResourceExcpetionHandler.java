8674c7415e094d2c3f3bdc88276bc1d3a9102a9a
Evandro Lima <evandrotiangua@gmail.com>
2018-02-22 10:07:45 -0300

    Deletando uma Categoria com Delete

--- src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExcpetionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExcpetionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -18,6 +19,12 @@ public class ResourceExcpetionHandler {
 	public ResponseEntity<StandardError> objectNotFound (ObjectNotFoundException e, HttpServletRequest request){
 		 StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+    
+    @ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity (DataIntegrityException e, HttpServletRequest request){
+		 StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 
 }
